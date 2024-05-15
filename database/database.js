102b6570e324af91eb2eeb7b66fa304e3144172b
deepaksingh101101 <65071552+deepaksingh101101@users.noreply.github.com>
2024-05-15 09:48:07 +0530

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectToMongo=async()=>{
 try {
-   const conn= await mongoose.connect("mongodb://127.0.0.1:27017/DB")
+   const conn= await mongoose.connect("mongodb+srv://deepaksingh101101:wUQmP2MAnhmcDEY1@cluster0.moyw7tw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
    console.log("Database Connected")
 } catch (error) {
     console.log(error)
